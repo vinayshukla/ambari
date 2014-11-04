c1b0d25afc83490982238f2d30c4699afffed121
Siddharth Wagle <swagle@hortonworks.com>
2014-11-03 17:24:42 -0800

    AMBARI-5707. Mini HBase cluster setup for unit testing. (swagle)

--- ambari-metrics/ambari-metrics-hadoop-timelineservice/src/test/java/org/apache/hadoop/yarn/server/applicationhistoryservice/metrics/timeline/TestPhoenixTransactSQL.java
+++ ambari-metrics/ambari-metrics-hadoop-timelineservice/src/test/java/org/apache/hadoop/yarn/server/applicationhistoryservice/metrics/timeline/TestPhoenixTransactSQL.java
@@ -17,18 +17,14 @@
  */
 package org.apache.hadoop.yarn.server.applicationhistoryservice.metrics.timeline;
 
-import org.apache.hadoop.metrics2.sink.timeline.TimelineMetric;
-import org.apache.hadoop.yarn.util.timeline.TimelineUtils;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
-import java.util.Arrays;
-import java.util.HashMap;
 
-import static org.apache.hadoop.yarn.server.applicationhistoryservice.metrics.timeline.AbstractTimelineAggregator.*;
+import java.util.Arrays;
+
 import static org.apache.hadoop.yarn.server.applicationhistoryservice.metrics.timeline.PhoenixTransactSQL.Condition;
 
-public class TestPhoenixTransactSQL {
+public class TestPhoenixTransactSQL extends AbstractMiniHBaseClusterTest {
   @Test
   public void testConditionClause() throws Exception {
     Condition condition = new Condition(
@@ -36,10 +32,12 @@ public class TestPhoenixTransactSQL {
         1407959718L, 1407959918L, null, false);
 
     String preparedClause = condition.getConditionClause();
-    String expectedClause = "METRIC_NAME IN (?,?) AND HOSTNAME = ? AND APP_ID" +
-      " = ? AND INSTANCE_ID = ? AND START_TIME >= ? AND START_TIME < ?";
+    String expectedClause = "METRIC_NAME IN (?, ?) AND HOSTNAME = ? AND " +
+      "APP_ID = ? AND INSTANCE_ID = ? AND SERVER_TIME >= ? AND SERVER_TIME < ?";
 
     Assert.assertNotNull(preparedClause);
     Assert.assertEquals(expectedClause, preparedClause);
   }
+
+
 }
